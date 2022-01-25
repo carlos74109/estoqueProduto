aa27612a5d6cc89c250026e584a06156a602ccf8
Carlos <carlos-84@live.com>
2022-01-24 21:11:22 -0300

    ________

---
+++ Produto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueProduto.Models
+{
+    internal class Produto
+    {
+
+        public string nome;
+        public double preco;
+        public int quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public double ValorTotalEstoque()
+        {
+            double media = preco * quantidade;
+            return media;
+        }
+    
+        public void AdicionarQuantidade(int qtde)
+        {
+            quantidade += qtde; 
+        }
+
+        public void RemoverProduto(int qtde)
+        {
+            quantidade -= qtde;
+        }
+
+        public override string ToString()
+        {
+            return "A quantidade do seu"
+            +  " estoque é de " + quantidade + " produtos, "
+            + "e o valor total desse estoque é " + ValorTotalEstoque();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using EstoqueProduto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o nome do produto: ");
+            string nomeProd = Console.ReadLine();
+
+            Console.WriteLine("Valor do produto: ");
+            double valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantidade do produto no estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            Produto prod = new Produto(nomeProd, valor, qtd);
+            Console.WriteLine(prod);
+
+            Console.WriteLine("1-Adicione mais produtos");
+            Console.WriteLine("2-Remova os produtos");
+            Console.WriteLine("3-Fechar aplicação");
+            string selecionarOpc = Console.ReadLine();
+
+
+
+            while (selecionarOpc == "1" || selecionarOpc == "2")
+            {
+                if (selecionarOpc == "1")
+                {
+                    Console.WriteLine("Adicione mais produtos ao estoque");
+                    int adicionarProd = int.Parse(Console.ReadLine());
+                    prod.AdicionarQuantidade(adicionarProd);
+
+                    Console.WriteLine("estoque atual após a inclusão de mais produtos, " + prod);
+
+                    Console.WriteLine("1-Adicione mais produtos");
+                    Console.WriteLine("2-Remova os produtos");
+                    Console.WriteLine("3-Fechar aplicação");
+                    selecionarOpc = Console.ReadLine();
+
+                }
+                else if (selecionarOpc == "2")
+                {
+                    Console.WriteLine("Quantidades de produtos removidos");
+                    int removerProd = int.Parse(Console.ReadLine());
+                    prod.RemoverProduto(removerProd);
+
+                    Console.WriteLine("Após remoção dos produtos, " + prod);
+
+                    Console.WriteLine("1-Adicione mais produtos");
+                    Console.WriteLine("2-Remova os produtos");
+                    Console.WriteLine("3-Fechar aplicação");
+                    selecionarOpc = Console.ReadLine();
+                }
+               
+            }
+             
+            
+                Console.WriteLine("aplicação encerrada");
+            
+
+        }
+    }
+}
